07026fd4a72b83e376374cbcb09243246a5a6425
James Thorpe <james.thorpe@utexas.edu>
2017-12-21 17:23:58 -0600

    Changed Ftab to binary file, and Boys function read to unformatted read. 100-fold speedup :)

---
+++ src/integrals/test.f90
@@ -0,0 +1,14 @@
+program test
+  implicit none
+
+  REAL(KIND=8), DIMENSION(0:120,0:22) :: Ftab
+
+  OPEN(unit=1,file='old-Ftab',status='old',access='sequential')
+  READ(1,*) Ftab
+  CLOSE(unit=1)
+
+  OPEN(unit=2,file='Ftab',status='replace',access='sequential',form='unformatted')
+  WRITE(2) Ftab
+  CLOSE(unit=2)
+
+end program test
