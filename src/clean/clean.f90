157b8b392791e04fe31accaddfc659c38a7f9e6a
James Thorpe <james.thorpe@utexas.edu>
2018-01-21 16:27:18 -0500

    Added a (crummy) make system

---
+++ src/clean/clean.f90
@@ -0,0 +1,15 @@
+!///////////////////////////////////////////////////////////////////
+!//             Program for running Hartree Fock QC calcualations
+!//
+!//                     James H Thorpe, in Group of John Stanton
+!//                     The University of Florida
+!//
+!///////////////////////////////////////////////////////////////////
+
+PROGRAM clean
+  IMPLICIT NONE
+
+  CALL EXECUTE_COMMAND_LINE('rm fmem envdat nucpos radii error basinfo &
+    Suv Huv Sold Hold setinfo XX Da dens.txt Guv 2> /dev/null')
+
+END PROGRAM clean
--- src/scf/maker.sh
+++
@@ -1,1 +0,0 @@
-gfortran -o ../../bin/scf -O3 -L/Users/jamesthorpe/LAPACK/lapack-3.7.0 -llapack -lblas scf.f90 ../env.f90 ../basis.f90 
